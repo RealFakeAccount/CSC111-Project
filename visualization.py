887d4d5126fd9eae7ef6f5b8baa3fa74f68e5e28
ReakFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2021-03-26 16:36:28 -0400

    3.26

--- visualization.py
+++ visualization.py
@@ -1,18 +1,49 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+from plotly.graph_objs import Scatter, Figure
+import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-def generate_title() -> None:
-    return html.H1
 
-def generate_layout() -> None:
-    ele = list()
-    ele.append()
-    app.layout = html.Div()
+def graph_figure() -> Figure():
+    """create a figure object based on given variables. 
+    See a3_visualization.py for examples.
+    The following code is just a example and (TODO) SHOULD be modified 
+    """
+    fig = px.line(x=["a","b","c"], y=[1,3,2], title="sample figure")
+    return fig
 
-if __name__ == 'main':
+ele = [
+    # Title
+    html.H3(children="Animmend - An interactive anime recommendation system", style={"textAlign": "center"}),
+
+    # Text Input
+    html.Label("Text Input"),
+    dcc.Input(id = "name", value = "Oregairu", type = "text"),
+
+    # Graph
+    dcc.Graph(
+        id = "connection-graph",
+        Figure = graph_figure()
+    )
+]
+app.layout = html.Div(children=ele)
+
+
+
+@app.callback(
+    Input(component_id="name", component_property="value")
+)
+def update_using_input_name(value):
+    '''TODO: change the graph and whatever based on the user input
+
+    '''
+    ...
+
+
+if __name__ == '__main__':
     app.run_server(debug=True)
