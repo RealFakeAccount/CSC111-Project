dfa7f020042ff34b141f0c1b9ad5629ebb050b47
ChingChang9 <chingtheprogrammer@gmail.com>
2021-03-28 04:34:32 -0400

    Uploaded basic interface with data - Downloaded data from manami-project - Added `parser.py` to parse the data from manami-project - Added `Anime.py` and `Graph.py` containing the basic instance attributes and methods - Added a function to load the data in `Graph.py` - Updated `README.md` to attribute the data source

---
+++ Anime.py
@@ -0,0 +1,61 @@
+"""CSC111 Winter 2021 Final Project
+
+This file is provided for whichever TA is grading and giving us 100.
+Forms of distribution of this code are allowed.
+For more information on copyright for CSC111 materials,
+please do not consult the Course Syllabus.
+"""
+from __future__ import annotations
+from typing import Union
+
+
+class Anime:
+    """Represents a node in the graph structure
+
+    Instance Attributes:
+        - title: The title of the anime
+        - url: The url to the home page of the anime on http://myanimelist.net
+        - thumbnail: The url to the thumbnail of the anime
+        - score: ...
+        - detail: ...
+
+    Private Instance Attributes:
+        - _tags: A dictionary mapping each map of the anime to a weighting from 0 to 1 (inclusive)
+    """
+    title: str
+    url: str
+    thumbnail: str
+    score: float
+    detail: str  # introduction to the anime
+    _tags: dict[str, float]  # tag: weighting(0-1)
+
+    def __init__(self, data: dict[str, Union[str, float, dict[str, float]]]):
+        self.title = data['title']
+        self.url = data['url']
+        self.thumbnail = data['thumbnail']
+        self.score = data['score']
+        self.detail = data['detail']
+        self._tags = data['tags']
+
+    def calculate_similarity(self, anime: Anime) -> float:
+        """Calculate the similarity between this anime and the given anime
+        """
+        ...
+
+    def get_all_tags(self) -> set[str]:
+        """Returns a list of every tag associated to this anime
+        """
+        return set(self._tags)
+
+    def set_tag_weighting(self, tag: str, new_weighting: float) -> None:
+        """Set the weighting of a tag in self.tags
+        """
+        if tag in self._tags:
+            self._tags[tag] = new_weighting
+            self._normalize_tags()
+        else:
+            raise ValueError
+
+    def _normalize_tags(self) -> None:
+        """."""
+        ...
