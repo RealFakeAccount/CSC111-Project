d460fdb2a86a03da5757ec48630d3c35699559a5
ChingChang9 <chingtheprogrammer@gmail.com>
2021-04-11 18:54:17 -0400

    Added `get_all_anime` to avoid accessing private attributes - Fixed some minor pyta styling errors

--- Graph.py
+++ Graph.py
@@ -76,7 +76,7 @@ class Graph:
         """
         G.add_node(det_anime_title, kind=str)
         G.add_edge(cur_anime_title, det_anime_title)
-    
+
     def _get_all_edges_pos(self, G: nx.Graph, nxg: dict):
         """Get all edges position in networkx graph and return a tuple of edges position in x-y dimension
         """
@@ -92,27 +92,27 @@ class Graph:
             x_mid_pos.extend([(x0 + x1) / 2, None])
             y_mid_pos.extend([(y0 + y1) / 2, None])
         return (x_edge_pos, y_edge_pos, (x_mid_pos, y_mid_pos))
-        
+
     def draw_graph(self, anime_title: str, depth: int, limit: int) -> plotly.graph_objs.Figure():
         """Draw a plotly graph centered around the given anime title
         Preconditions:
             - depth <= 5 # This will be handled by the slider on the website
         """
-        edge = dict()  #dict[Tuple[str, str], float]
+        edge = dict()  # dict[Tuple[str, str], float]
         node = dict()
 
         G = nx.Graph()
-        shell = [[anime_title], []] #[[center of graph], [other nodes]]
+        shell = [[anime_title], []] # [[center of graph], [other nodes]]
         Q = [(anime_title, 0)]
         while len(Q) != 0:
             cur = Q[0]
             shell[1].append(cur[0])
             Q.pop(0)
-            
+
             for i in self.get_related_anime(cur[0], limit):
                 self.add_connection(G, cur[0], i.title)
                 if cur[1] < depth: Q.append((i.title, cur[1] + 1))
-            
+
         print(len(shell[1]))
 
         if 1 + limit ** depth > 3:
@@ -127,22 +127,22 @@ class Graph:
         all_traces = []
 
         nodes_trace = plotly.graph_objs.Scatter(
-            x = x_node_pos,
-            y = y_node_pos,
-            mode = "markers",
-            name = "nodes",
+            x=x_node_pos,
+            y=y_node_pos,
+            mode="markers",
+            name="nodes",
             marker={'size': 50, 'color': 'LightSkyBlue'}
         )
 
         all_traces.append(nodes_trace)
 
         edges_trace = plotly.graph_objs.Scatter(
-            x = x_edge_pos,
-            y = y_edge_pos,
-            mode = "lines",
-            name = "edges",
-            line = dict(color = 'rgb(210,210,210)', width = 1),
-            hoverinfo = "none"
+            x=x_edge_pos,
+            y=y_edge_pos,
+            mode="lines",
+            name="edges",
+            line=dict(color='rgb(210,210,210)', width=1),
+            hoverinfo="none"
         )
 
         all_traces.append(edges_trace)
@@ -164,13 +164,12 @@ class Graph:
             xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
             yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False}
         )
-        
+
         figure = plotly.graph_objs.Figure(data=all_traces, layout=graph_layout)
 
         print("updated")
 
         return figure
-        
 
     def adjust_weighting_v1(self, anime: Anime, tag: str, reaction: str = 'upvote') -> None:
         """
@@ -253,10 +252,10 @@ def load_anime_graph(file_name: str) -> Graph:
         for title in data:
             anime_graph.add_anime(title, data[title])
 
-    cnt = 0
-    for a in anime_graph._anime.keys():
-        anime_graph.calculate_neighbours(anime_graph._anime[a])
-        cnt += 1
-        print(f"done {cnt}")
+    count = 0
+    for anime_name in anime_graph.get_all_anime():
+        anime_graph.calculate_neighbours(anime_name)
+        count += 1
+        print(f"done {count}")
 
     return anime_graph
