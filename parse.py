8f3e36d6a451dc7133b1539c54028c99b61ae6f4
ChingChang9 <chingtheprogrammer@gmail.com>
2021-04-11 18:45:46 -0400

    Improved parser - Only parse 1000 anime - Include anime descriptions

--- parse.py
+++ parse.py
@@ -10,28 +10,50 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_json(file_name: str, output_file: str) -> None:
+def parse_json(file_name: str, output_file: str, verbose=True, start=12500, end=13500) -> None:
     """Parse the anime dataset from file_name and write the output to output_file"""
     new_data = {}
 
     with open(file_name) as original_file:
         data = json.load(original_file)
-        for anime in data['data']:
-            page = requests.get(anime['sources'][0])
-            soup = BeautifulSoup(page.content, 'html.parser')
-            detail_raw = soup.find(itemprop='description')
-            if detail_raw is None:
-                detail = 'No details found for this anime'
-            else:
-                detail = detail_raw.text
+        for i in range(start, end):
+            anime = data['data'][i]
+            description = get_anime_description(anime['sources'][0])
 
             new_data[anime['title']] = {
                 'title': anime['title'],
                 'url': anime['sources'][0],
                 'thumbnail': anime['picture'],
                 'tags': anime['tags'],
-                'detail': detail
+                'detail': description
             }
+            if verbose:
+                print(f'{round((i - start) / (end - start) * 100, 2)}%')
 
         with open(output_file, 'w') as new_file:
             json.dump(new_data, new_file)
+
+
+def get_anime_description(url: str) -> str:
+    """Scrape the description/synopsis of an anime from the given url
+    """
+    page = requests.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    })
+    soup = BeautifulSoup(page.content, 'html.parser')
+    if url.startswith('https://anidb.net'):
+        detail_raw = soup.find(itemprop='description')
+    elif url.startswith('https://myanimelist.net'):
+        detail_raw = soup.find(itemprop='description')
+    elif url.startswith(' https://kitsu.io'):
+        detail_raw = soup.find(id='ember66')
+    elif url.startswith('https://anime-planet.com'):
+        detail_raw = soup.find('div', class_='md-3-5').find('p')
+    else:
+        detail_raw = None
+
+    if detail_raw is not None:
+        return detail_raw.text
+    else:
+        print(f'Failed: {url}')
+        return 'No details found for this anime'
