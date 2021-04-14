ef61d2da5db6df0b7886eec6bbd50c203892be74
MakoStrwlkr <72935357+MakoStrwlkr@users.noreply.github.com>
2021-04-15 01:23:56 +0530

    Add prediction system

--- Anime.py
+++ Anime.py
@@ -4,11 +4,12 @@ Forms of distribution of this code are allowed.
 For more information on copyright for CSC111 materials,
 please do not consult the Course Syllabus.
 
+Copyright (c) 2021 by Ching Chang, Letian Cheng, Arkaprava Choudhury, Hanrui Fan
+
 TODO: Improve docstrings
 TODO: Custom errors
 TODO: Research efficiency / make more efficient implementation
 TODO: different levels of strictness
-
 """
 from __future__ import annotations
 from typing import Union
@@ -17,26 +18,30 @@ from math import sqrt
 
 NORMALIZATION_SCALE = 1.0
 NORMALIZATION_CONSTANT = sqrt(NORMALIZATION_SCALE)
-NEIGHBOR_LIMIT = 5
+NEIGHBOUR_LIMIT = 5
+
 
 class Anime:
-    """Represents a node in the graph structure
+    """
+    Represents a node in the graph structure, i.e. represents data about a single anime
+
     Instance Attributes:
         - title: The title of the anime
         - url: The url to the home page of the anime on http://myanimelist.net
         - thumbnail: The url to the thumbnail of the anime
         - detail: ...
-        - neighbours: ...
-
-    Private Instance Attributes:
-      - _tags: A dictionary mapping each tag of the anime to a weighting from 0 to 1 (inclusive)
+        - neighbours: A list of anime that are similar to this anime
     """
+
     title: str  # name of anime
-    url: str
+    url: str  # url of anime on MAL
     thumbnail: str  # thumbnail
     score: float  # average rating in the database
     detail: str  # introduction to the anime
     neighbours: list[Anime]
+
+    # Private Instance Attributes:
+    #   - _tags: A dictionary mapping each tag of the anime to a weighting from 0 to 1 (inclusive)
     _tags: dict[str, float]  # tag: weighting(0-1)
 
     def __init__(self, data: dict[str, Union[str, list[str]]]):
@@ -50,11 +55,8 @@ class Anime:
 
     def calculate_similarity(self, anime: Anime) -> float:
         """Calculate the similarity between this anime and the given anime.
-
         The similarity of two anime is defined to be the dot product of their
-
         Version 1: use numpy's dot product; assumes not many tags, so minimal error.
-
         Version 2: use self-defined functions to find dot product. Slower, but more reliable.
         """
         # VERSION 1.
@@ -72,9 +74,25 @@ class Anime:
         return similarity
 
     def get_all_tags(self) -> set[str]:
-        """Returns a list of every tag associated to this anime
+        """
+        Return the set of every tag associated with this anime
         """
         return set(self._tags)
+
+    def get_tags(self) -> dict[str, float]:
+        """
+        Return the tags and their weights attached to this anime.
+        """
+        return self._tags
+
+    def get_tag_weight(self, tag: str) -> float:
+        """
+        Return the tags and their weights attached to this anime.
+        """
+        if tag in self._tags:
+            return self._tags[tag]
+        else:
+            raise ValueError
 
     def set_tag_weighting(self, tag: str, new_weighting: float) -> None:
         """Set the weighting of a tag in self.tags
@@ -87,7 +105,6 @@ class Anime:
 
     def _normalize_tags(self) -> None:
         """Normalize all tags so that the real Euclidean norm of the total tag vector is 1.
-
         In other words, divide the weight of each tag by the sum of the squares of all weights.
         """
         # VERSION 1.
@@ -102,29 +119,29 @@ class Anime:
         # VERSION 2.
         normalize_dict(self._tags)
 
-    def get_tags(self) -> dict[str, float]:
-        """Return the tags attached to self.
-        """
-        return self._tags
-
     def insert_neighbour(self, anime: Anime) -> None:
         """bleh"""
         self.neighbours.append(anime)
         self.neighbours.sort(key=self.calculate_similarity, reverse=True)
-        self.neighbours = self.neighbours[:NEIGHBOR_LIMIT]
+
+        # TODO: do we need the following line at all?
+        # self.neighbours = self.neighbours[:NEIGHBOUR_LIMIT]
+
         # if self.neighbours == []:
         #     self.neighbours.append(anime)
         #     return None
 
         # n = len(self.neighbours)
 
-        # while self.calculate_similarity(anime) > self.calculate_similarity(anime.neighbours[n]) and n > 0:
+        # while self.calculate_similarity(anime) > self.calculate_similarity(anime.neighbours[n])
+        # and n > 0:
         #     n -= 1
 
         # self.neighbours.insert(n, anime)
 
     def adjust_negative_feedback(self, anime: Anime) -> None:
-        """Readjust the weightings upon receiving negative feedback for similarity with anime.
+        """
+        Readjust the weightings upon receiving negative feedback for similarity with anime.
         """
         for tag in self._tags:
             if tag in anime._tags:
@@ -133,7 +150,8 @@ class Anime:
         normalize_dict(self._tags)
 
     def adjust_positive_feedback(self, anime: Anime) -> None:
-        """Readjust the weightings upon receiving positive feedback for similarity with anime.
+        """
+        Readjust the weightings upon receiving positive feedback for similarity with anime.
         """
         for tag in self._tags:
             if tag in anime._tags:
@@ -141,10 +159,22 @@ class Anime:
 
         normalize_dict(self._tags)
 
+    def prediction_similarity(self, prediction_weights: dict[str, float]) -> float:
+        """
+        Return how likely the user is to pick anime. This is NOT a measure of probability, and
+        is hence not normalized.
+        """
+        sum_so_far = 0
+        for tag in self.get_tags():
+            if tag in prediction_weights:
+                sum_so_far += prediction_weights[tag]
+        return sum_so_far
+
 
 def normalize_dict(values: dict[str, float]) -> None:
-    """This function mutates the values dict such that the sum of the squares of all the values
-    is NORMALIZATION_CONSTANT
+    """
+    Mutate the values of dict such that the sum of the squares of all the values
+    is NORMALIZATION_CONSTANT.
     """
     sum_of_squares = 0
 
@@ -156,7 +186,12 @@ def normalize_dict(values: dict[str, float]) -> None:
 
 
 def _initialize_tags(tags: list[str]) -> dict[str, float]:
-    """Initializes the tags in anime so that they all have the same weight."""
+    """
+    Given a list of tags, return a dict with <tag>:<value> pairs such that the values corresponding
+    to each tag are the same and the sum of the squares of values is  NORMALIZATION_SCALE.
+
+    This is used to initialize the tags in anime so that they all have the same weight.
+    """
     anime_tags = {}
 
     for tag in tags:
@@ -165,3 +200,19 @@ def _initialize_tags(tags: list[str]) -> dict[str, float]:
     normalize_dict(anime_tags)
 
     return anime_tags
+
+
+# if __name__ == "__main__":
+#     import python_ta
+#     python_ta.check_all(config={
+#         'max-line-length': 100,
+#         'disable': ['E1136'],
+#         'extra-imports': ['a1_linked_list'],
+#         'max-nested-blocks': 4
+#     })
+#
+#     import python_ta.contracts
+#     python_ta.contracts.check_all_contracts()
+#
+#     import doctest
+#     doctest.testmod()
