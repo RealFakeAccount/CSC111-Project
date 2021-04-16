59a1a764cb7e387642680dbf591771b597cc997a
ChingChang9 <chingtheprogrammer@gmail.com>
2021-04-16 06:57:08 -0400

    can't define functions in main.py

--- main.py
+++ main.py
@@ -5,38 +5,7 @@ For more information on copyright for CSC111 materials, please consult the Cours
 
 Copyright (c) 2021 by Ching Chang, Letian Cheng, Arkaprava Choudhury, Hanrui Fan
 """
-
-
-import parse
-from graph import LoadGraphFast
-import time
-
-
-def generate_dataset(file_name: str, output_folder: str) -> None:
-    """ The datasets are already provided in data.zip, but run this if you want to generate
-    the dataset from scratch.
-
-    In order to run this, you should provided the path to the original Manami dataset.
-    We have downloaded for you at ./data/original.json
-
-    Running time depends on your computer. It varies from 20 second to 18 minutes.
-    """
-    t = time.process_time()
-
-    parse.parse_json(file_name, output_folder + '/full.json', True, 0, 40000)
-    parse.parse_json(file_name, output_folder + '/small.json', True)
-
-    graph = LoadGraphFast().load_anime_graph_multiprocess(output_folder + '/small.json')
-    graph.serialize(output_folder + '/small_graph.json')
-    print('Finish writing to small_graph')
-
-    graph = LoadGraphFast().load_anime_graph_multiprocess(output_folder + '/full.json')
-    graph.serialize(output_folder + '/full_graph.json')
-    print('Finish writing to full_graph')
-
-    elapsed_time = time.process_time() - t
-    print(f'Dataset generation finished within {elapsed_time}s')
-
+from parse import generate_dataset
 
 if __name__ == '__main__':
-    generate_dataset('data/original.json', 'data')
+    generate_dataset('./data/original.json', 'data')
--- parse.py
+++ parse.py
@@ -6,6 +6,8 @@ For more information on copyright for CSC111 materials,
 please do not consult the Course Syllabus.
 """
 import json
+from graph import LoadGraphFast
+import time
 
 
 def parse_json(file_name: str, output_file: str, verbose: bool = True, start: int = 12500,
@@ -36,6 +38,32 @@ def parse_json(file_name: str, output_file: str, verbose: bool = True, start: in
         with open(output_file, 'w') as new_file:
             print(f'Writing to {output_file}.. {skipped} are skipped because of 0 tag')
             json.dump(new_data, new_file)
+
+
+def generate_dataset(file_name: str, output_folder: str) -> None:
+    """ The datasets are already provided in data.zip, but run this if you want to generate
+    the dataset from scratch.
+
+    In order to run this, you should provided the path to the original Manami dataset.
+    We have downloaded for you at ./data/original.json
+
+    Running time depends on your computer. It varies from 20 second to 18 minutes.
+    """
+    t = time.process_time()
+
+    parse_json(file_name, output_folder + '/full.json', True, 0, 40000)
+    parse_json(file_name, output_folder + '/small.json', True)
+
+    graph = LoadGraphFast().load_anime_graph_multiprocess(output_folder + '/small.json')
+    graph.serialize(output_folder + '/small_graph.json')
+    print('Finish writing to small_graph')
+
+    graph = LoadGraphFast().load_anime_graph_multiprocess(output_folder + '/full.json')
+    graph.serialize(output_folder + '/full_graph.json')
+    print('Finish writing to full_graph')
+
+    elapsed_time = time.process_time() - t
+    print(f'Dataset generation finished within {elapsed_time}s')
 
 
 if __name__ == '__main__':
